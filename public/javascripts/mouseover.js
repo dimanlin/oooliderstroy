91421a151590dc424b755554285a77b07856b10f
Diman <devandart@ya.ru>
2011-05-12 11:15:29 +0400

    Добавил файлы для вёрстки

---
+++ public/javascripts/mouseover.js
@@ -0,0 +1,15 @@
+/*======================================================================*\
+|| #################################################################### ||
+|| # Package - Joomla Template based on YJSimpleGrid Framework          ||
+|| # Copyright (C) 2010  Youjoomla LLC. All Rights Reserved.            ||
+|| # Authors - Dragan Todorovic and Constantin Boiangiu                 ||
+|| # license - PHP files are licensed under  GNU/GPL V2                 ||
+|| # license - CSS  - JS - IMAGE files  are Copyrighted material        ||
+|| # bound by Proprietary License of Youjoomla LLC                      ||
+|| # for more information visit http://www.youjoomla.com/license.html   ||
+|| # Redistribution and  modification of this software                  ||
+|| # is bounded by its licenses                                         ||
+|| # websites - http://www.youjoomla.com | http://www.yjsimplegrid.com  ||
+|| #################################################################### ||
+\*======================================================================*/
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('q.f("r",2(){s n=$("t");7=3 4();5=3 4();u=3 4();8=3 4();n.9().b(2(o,i){7[i]=3 4();5[i]=3 4();8[i]=3 4();o.v("w").b(2(6,j){7[i][j]=6.x();5[i][j]=3 z.A(6,{B:C,D:E});8[i][j]=6.F();6.c("d","g");5[i][j].p({h:0,m:0})})});7.b(2(e,k){e.b(2(a,l){a.f("G",2(){a.9()[1].c("d","g");5[k][l].H({h:1,m:8[k][l]["I"].y});(2(){a.9()[1].c("d","")}).J(K)});a.f("L",2(){a.9()[1].c("d","g");5[k][l].M();5[k][l].p({h:0,m:0})})})})});',49,49,'||function|new|Array|effects1|elm|levels|sizes|getChildren||each|setStyle|overflow||addEvent|hidden|opacity|||||height|main|el|set|window|domready|var|horiznav|effects2|getElementsBySelector|ul|getParent||Fx|Styles|wait|false|duration|300|getSize|mouseenter|start|size|delay|500|mouseleave|stop'.split('|'),0,{}))
